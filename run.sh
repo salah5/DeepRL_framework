2461a1d9e0f97354b058a779e728fcf2ab64d29e
Mohsen Mesgar <mohsen.mesgar@h-its.org>
2019-03-05 15:04:23 +0100

    update

--- run.sh
+++ run.sh
@@ -1,3 +1,42 @@
 #!/bin/bash
 
+###
+##
+# define variables
+machine='local' # possible values: {'local', 'gpu'}, where 'local' has no GPU and cuda
+ 
+###
+##
+# check if we are in RL virtual environment
+if [[ $VIRTUAL_ENV == '' ]]
+then
+
+    source activate rl
+fi
+
+###
+##
+# export the spec of the running environment (rl)
+conda list --explicit > ./documentation/spec-file-$machine.txt 
+
+###
+##
+# run the latest version of the model by getting it from GitHub
+if [[ $machine == 'gpu' ]]
+then
+	
+	git pull
+fi
+
+
+
+###
+##
+# Finally execute the model
 python run.py 
+
+
+###
+##
+# exit from virtual env
+source deactivate rl
